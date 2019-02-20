c685521a628e3ce8be5be3948eed7b0fd2be5fae
Rhyeca <Rhyeca@LcReCeEs>
2019-02-20 16:19:14 -0700

    First commit: Added javadoc comments and made no other changes to the code

---
+++ CSE360assign2/src/Calculator.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ * Assignment number 2 is meant for us to practice using github
+ * by committing and pushing code to github
+ * @author rjriley3 Class ID: 534
+ *
+ */
+public class Calculator 
+{
+	private int total;			
+	/**
+	 * constructor method
+	 */
+	public Calculator () 
+	{
+		total = 0; // not needed - included for clarity
+	}
+	/**
+	 * Return the current total from all the functions
+	 * @return the total
+	 */
+	public int getTotal () 
+	{
+		return 0;
+	}
+	/**
+	 * add the parameter value into the total variable
+	 * @param value, given input value
+	 */
+	public void add (int value) 
+	{
+
+	}
+	/**
+	 * subtract the parameter value from the total variable
+	 * @param value, given input value
+	 */
+	public void subtract (int value) 
+	{
+
+	}
+	/**
+	 * Multiply the total by the parameter 
+	 * @param value, given input value
+	 */
+	public void multiply (int value) 
+	{
+
+	}
+	/**
+	 * divide the total by the parameter
+	 * If value equals zero set the total equal to zero
+	 * @param value, given input value
+	 */
+	public void divide (int value) 
+	{
+
+	}
+	/**
+	 * Return a history of all actions performed (add, subtract, divide, multiply)
+	 * @return history
+	 */
+	public String getHistory () 
+	{
+		return "";
+	}
+}
